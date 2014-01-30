7fe68234b6a4439f09a467a814393587dcf66171
LaraTP <laratallantepas@gmail.com>
2014-01-30 23:59:24 +0100

    Update ajax.js

--- 4/ajax.js
+++ 4/ajax.js
@@ -1,24 +1,23 @@
 function enviarDatosEmpleado(){
 
-  nombre=document.form4.nombre.value;
-  telefono=document.form4.telefono.value;
+  nombre = document.form4.nombre.value;
+  telefono = document.form4.telefono.value;
 
-  ajax=objetoAjax();
+  ajax = objetoAjax();
 
   ajax.open("POST", "registro.php",true);
-  ajax.onreadystatechange=function() {
-  	if (ajax.readyState==3) {
-		divResultado.innerHTML = ajax.responseText
+  ajax.onreadystatechange = function() {
+  	if (ajax.readyState == 3) {
+		divResultado.innerHTML = ajax.responseText;
 		LimpiarCampos();
 	}
  }
 	ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-	ajax.send("nombre="+nombre+"&telefono="+telefono)
+	ajax.send("nombre = " + nombre + " & telefono = " + telefono);
 }
  
-//función para limpiar los campos
 function LimpiarCampos(){
-  document.form4.nombre.value="";
-  document.form4.telefono.value="";
+  document.form4.nombre.value = "";
+  document.form4.telefono.value = "";
   document.form4.nombre.focus();
 }
