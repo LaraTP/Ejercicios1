05d4e50ce26c8d86c18316d18ea6b476bf948f68
LaraTP <laratallantepas@gmail.com>
2014-01-31 17:58:41 +0100

    Subir archivos

---
+++ 5/Form5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<script language="JavaScript" type="text/javascript" src="ajax.js"></script>
+</head>
+<body>
+	<form name='form5' id='form5' action="SubirFichero.php" method="POST" enctype="multipart/form-data">
+		<input type="file" name="file" id="file"/>
+		</br>
+		<input type="submit" name="enviar" id="enviar" value="Enviar"/>
+	</form>
+</body>
+</html>
